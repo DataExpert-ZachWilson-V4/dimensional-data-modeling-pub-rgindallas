0c32af8c6bf03d572792fbd0c61c21926db9413b
rgindallas <richardgarth68@gmail.com>
2024-05-16 11:50:09 -0500

    Update query_1.sql

---
+++ submission/query_1.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE TABLE actors (
+  actor VARCHAR(255),
+  actor_id VARCHAR(255),
+  -- 'films': Array of ROWs for multiple films associated with each actor. Each row contains film details.
+  films ARRAY(
+    ROW(
+      -- 'film': Name of the film.
+      film VARCHAR(255),
+      -- 'votes': Number of votes the film received.
+      votes INTEGER,
+      -- 'rating': Rating of the film.
+      rating DOUBLE,
+      -- 'film_id': Unique identifier for each film.
+      film_id VARCHAR(255),
+      -- 'year': Release year of the film.
+      year INTEGER
+    )
+  ),
+  -- 'quality_class': Categorical rating based on average rating in the most recent year.
+  quality_class VARCHAR(255),
+  -- 'is_active': Indicates if the actor is currently active, based on making films this year.
+  is_active BOOLEAN,
+  -- 'current_year': Represents the year this row is relevant for the actor.
+  current_year INTEGER
+)
+WITH(
+  -- Data stored in PARQUET format for optimized analytics.
+  format='PARQUET',
+  -- Partitioned by 'current_year' for efficient time-based analysis.
+  partitioning=array['current_year']
+)
